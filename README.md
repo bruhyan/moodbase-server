4eb4904eda734b6bad64d3b4f95f87a440435ac4
bryankoh <bryan_koh@mom.gov.sg>
2021-10-23 15:42:48 +0800

    feat: health check get API

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ go run main.go
 ```
 curl localhost:9090/health
 ```
+
+### Self learning help
+
+- `&` goes in front of a variable when you want to get that variable's memory address, i.e. gives a memory address value
+- `*` goes in front of a variable that holds a memory address and resolves it (counterpart of `&`). i.e. gets the value given a memory address variable
+- `*` goes in front of a type --> becomes part of type declaration --> "var str_pointer \*string: this variable holds a pointer to a string"
--- handlers/health.go
+++ handlers/health.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -14,6 +15,15 @@ func NewHealth(logger *log.Logger) *Health {
 	return &Health{logger}
 }
 
+// HTTP controller
 func (health *Health) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if(r.Method == http.MethodGet) {
+		health.logger.Println("Handle Health GET")
+		d, err := json.Marshal("OK");
+		if err != nil {
+			http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+		}
+		rw.Write(d)
+	}
 	health.logger.Println("OK")
 }
